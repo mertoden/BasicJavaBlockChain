8533d49e5287499ce58e05cd92269aea9e1825a5
Mert Öden <93067736+mertoden@users.noreply.github.com>
2022-05-13 23:30:48 +0300

    ödev tamamlandı

---
+++ Mert Öden BZ - b191200018/BlokZincir/src/BlokChain.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+public class BlokChain {
+	public static void main(String[] args) 
+	{
+		
+		ArrayList<Blok> blockChain = new ArrayList<Blok>();
+		
+		//ilk blok
+		String[] initialValues = {"mert has $700", "hasan has $550"};
+		Blok firstBlock = new Blok(initialValues,0);
+		blockChain.add(firstBlock);
+		System.out.println("ilk blok : " + firstBlock.toString());
+		System.out.println("Blok Zincir: " + blockChain.toString());
+		
+		//ikinci blok
+		String[] initialValues2 = {"mert gives oguzhan $405", " mert gives burak $50", "mert gives omer $45"};
+		Blok secondBlock = new Blok(initialValues2,0);
+		blockChain.add(secondBlock);
+		System.out.println("ikinci blok : " + secondBlock.toString());
+		System.out.println("Blok Zincir: " + blockChain.toString());
+		
+		//üçüncü blok
+				String[] initialValues3 = {"oguzhan gives mert $405", "hasan gives $60 to ömer"};
+				Blok thirdBlock = new Blok(initialValues2,0);
+				blockChain.add(thirdBlock);
+				System.out.println("üçüncü blok : " + thirdBlock.toString());
+				System.out.println("Blok Zincir: " + blockChain.toString());
+
+	}
+	
+
+}
